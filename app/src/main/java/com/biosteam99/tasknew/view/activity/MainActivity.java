4f03ccacfc914db4e829013448111715ff0eb724
mohammednahabbar <mohammed.na.habbar@gmail.com>
2022-03-17 20:59:28 +0300

    TaskNew

--- app/src/main/java/com/biosteam99/tasknew/view/activity/MainActivity.java
+++ app/src/main/java/com/biosteam99/tasknew/view/activity/MainActivity.java
@@ -1,8 +1,10 @@
-package com.biosteam99.tasknew;
+package com.biosteam99.tasknew.view.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.biosteam99.tasknew.R;
 
 public class MainActivity extends AppCompatActivity {
 
